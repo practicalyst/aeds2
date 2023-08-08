527660e406216b3850b084cfe0e9c3fe668733f6
Arthur Faria <af.arthurfaria@gmail.com>
2023-08-08 18:17:00 -0300

    Testa os limites de alguns tipos primitivos

---
+++ trabalhos/teoricos/nivelamento/LimiteVariaveis.java
@@ -0,0 +1,15 @@
+class LimiteVariaveis {
+	public static void main (String[] args){		
+		byte b = 0;
+		short s = 0;
+		int i = 0;
+		long l = 0;
+
+		while(true){
+			b++; s++; i++; l++;
+			System.out.println(b + " " + s + " " + i + " " + l);
+		
+		}
+	}
+}
+
